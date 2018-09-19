87b9556d57a1fb5ff9e676f7126ad931a9fba18b
ezekiel <dbzraccoon@icloud.com>
2018-09-19 19:35:17 -0400

    my first commit

---
+++ GuessMe.cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GuessMe.cli
+{
+    class Program
+    {
+        private static string name;
+        
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            Console.ReadKey();
+            
+        }
+    }
+}
